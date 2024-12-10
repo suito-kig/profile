59426ddfff1a52d8a5fcc301ecb6ba12106441e9
suito-kig <zebeapmq@gmail.com>
2024-12-10 21:52:35 +0900

    placed some files

---
+++ README.md
@@ -0,0 +1,21 @@
+# Profile
+
+## 一言
+
+ほぼ何も考えずにCopilotに指示したらjsができた
+
+楽ちん～
+
+## 経験(?)等
+
+- [見たアニメ](md/anime.md)
+- [プレイしたゲーム](md/game.md)
+- [読んだ漫画](md/manga.md)
+- [見た映画](md/movie.md)
+
+放送年、発売年、掲載年、上映年基準です
+
+## リンク集
+
+- [X(Twitter)](https://x.com/suito_kig)
+- [Bluesky](https://bsky.app/profile/suito-kig.bsky.social)
---
+++ src/json-to-md.js
@@ -0,0 +1,66 @@
+const fs = require('fs');
+const path = require('path');
+
+const files = [
+    { json: 'anime.json', md: 'anime.md', heading: '見たアニメ' },
+    { json: 'game.json', md: 'game.md', heading: 'プレイしたゲーム' },
+    { json: 'manga.json', md: 'manga.md', heading: '読んだ漫画' },
+    { json: 'movie.json', md: 'movie.md', heading: '見た映画' }
+];
+
+files.forEach(file => {
+    const jsonFilePath = path.join(__dirname, '..', 'data', file.json);
+    const mdFilePath = path.join(__dirname, '..', 'md', file.md);
+
+    fs.readFile(jsonFilePath, 'utf8', (err, data) => {
+        if (err) {
+            console.error(`Error reading file ${jsonFilePath}:`, err);
+            return;
+        }
+
+        let itemList;
+        try {
+            itemList = JSON.parse(data);
+        } catch (parseErr) {
+            console.error(`Error parsing JSON from file ${jsonFilePath}:`, parseErr);
+            return;
+        }
+
+        if (!Array.isArray(itemList)) {
+            console.error(`Error: JSON data in file ${jsonFilePath} is not an array`);
+            return;
+        }
+
+        const markdown = generateMarkdown(itemList, file.heading);
+
+        fs.writeFile(mdFilePath, markdown, (err) => {
+            if (err) {
+                console.error(`Error writing file ${mdFilePath}:`, err);
+                return;
+            }
+            console.log(`Markdown file ${mdFilePath} has been saved.`);
+        });
+    });
+});
+
+function generateMarkdown(itemList, heading) {
+    const years = {};
+    itemList.forEach(item => {
+        if (!years[item.year]) {
+            years[item.year] = [];
+        }
+        years[item.year].push(item.title);
+    });
+
+    let markdown = `# ${heading}\n\n計${itemList.length}作品\n\n`;
+    const sortedYears = Object.keys(years).sort((a, b) => b - a);
+    sortedYears.forEach(year => {
+        markdown += `## ${year}年\n\n`;
+        years[year].forEach(title => {
+            markdown += `- ${title}\n`;
+        });
+        markdown += "\n";
+    });
+
+    return markdown;
+}
